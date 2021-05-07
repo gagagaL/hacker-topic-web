e4d8acff04c675d938393657c19ce2d16dd2a406
masaaki-kawaguchi <masaaki.kawaguchi@fox-hound.jp>
2021-05-07 14:17:54 +0900

    ビルド時のエラー解消

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,5 +44,10 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     transpile: [/^element-ui/],
+    babel: {
+      presets({ isServer }, [preset, options]) {
+        options.loose = true
+      },
+    },
   }
 }
